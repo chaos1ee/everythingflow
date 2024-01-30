2facc843f26284c1996f59bb60719a4c4f96619c
hao.li <hao.li@funplus.com>
2024-01-30 18:53:13 +0800

    feat: export types

--- packages/handsontable/src/index.ts
+++ packages/handsontable/src/index.ts
@@ -2,5 +2,6 @@ import DiffTable from './DiffTable'
 import Table from './Table'
 import { useHeight } from './hooks'
 
+export type { TableProps, TableRef } from './Table'
 export { DiffTable, useHeight }
 export default Table
