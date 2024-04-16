e6e795627eec36de45977cc7dcf745a94d18aae8
hao.li <hao.li@funplus.com>
2024-04-16 17:18:08 +0800

    refactor: custom swr key serialize and deserialize

--- packages/react-toolkits/src/stores/queryList.ts
+++ packages/react-toolkits/src/stores/queryList.ts
@@ -1,71 +1,70 @@
+import type { QueryListDataType, QueryListProps } from '../components/QueryList'
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { mutate } from 'swr'
 import type { MutatorCallback, MutatorOptions } from 'swr/_internal'
 import { create } from 'zustand'
-import type { QueryListDataType, QueryListProps } from '../components/QueryList'
-import { getSwrKey } from '../components/QueryList'
+import { genSwrKey } from '../utils/queryList'
 
 type QueryListMutator = <T = any>(
-  key: string,
+  action: string,
   data?: QueryListDataType<T> | Promise<QueryListDataType<T>> | MutatorCallback<QueryListDataType<T>>,
   opts?: MutatorOptions<QueryListDataType<T>>,
 ) => void
 
-export interface QueryListPayload<Arg = any> {
+export interface QueryListPayload<FormValues = any> {
   page?: number
   size?: number
-  arg?: Arg
+  formValues?: FormValues
 }
 
 export interface QueryListState {
-  keyMap: Map<string, string | null>
+  swrKeyMap: Map<string, string | null>
   payloadMap: Map<string, QueryListPayload>
-  propsMap: Map<string, QueryListProps<any>>
+  propsMap: Map<string, QueryListProps>
   mutate: QueryListMutator
-
   setPayload(key: string, payload: QueryListPayload, forceUpdate?: boolean): void
 }
 
 export const useQueryListStore = create<QueryListState>((set, get) => ({
-  keyMap: new Map(),
+  swrKeyMap: new Map(),
   paginationMap: new Map(),
   valueMap: new Map(),
   payloadMap: new Map(),
   propsMap: new Map(),
-  setPayload(key, payload, forceUpdate = true) {
-    const map = get().payloadMap
+  setPayload(action, payload, forceUpdate = true) {
+    const payloadMap = get().payloadMap
 
     const newPayload = {
-      page: payload.page ?? map.get(key)?.page,
-      size: payload.size ?? map.get(key)?.size,
+      page: payload.page ?? payloadMap.get(action)?.page,
+      size: payload.size ?? payloadMap.get(action)?.size,
       arg: {
-        ...map.get(key)?.arg,
-        ...payload.arg,
+        ...payloadMap.get(action)?.formValues,
+        ...payload.formValues,
       },
     }
 
     if (forceUpdate) {
-      if (get().keyMap.get(key)) {
-        const prevSwrKey = get().keyMap.get(key) as string
-        const nextSwrKey = getSwrKey(key, newPayload, get().propsMap.get(key)?.params, get().propsMap.get(key)?.onePage)
+      if (get().swrKeyMap.get(action)) {
+        const prevSwrKey = get().swrKeyMap.get(action) as string
+        const nextSwrKey = genSwrKey(get().propsMap.get(action) as QueryListProps, newPayload)
 
         if (prevSwrKey === nextSwrKey) {
-          map.set(key, newPayload)
+          payloadMap.set(action, newPayload)
           // 因为 SWR key 没有变化时会使用缓存数据，所以需要调用 mutate 强行更新缓存。
           mutate(prevSwrKey, undefined, true)
         } else {
-          set({ payloadMap: new Map(map).set(key, newPayload) })
+          set({ payloadMap: new Map(payloadMap).set(action, newPayload) })
         }
       } else {
-        set({ payloadMap: new Map(map).set(key, newPayload) })
+        set({ payloadMap: new Map(payloadMap).set(action, newPayload) })
       }
     } else {
-      map.set(key, newPayload)
+      payloadMap.set(action, newPayload)
     }
   },
-  mutate: (key, data, opts) => {
-    const { keyMap } = get()
-    const swrKey = keyMap.get(key)
+  mutate: (action, data, opts) => {
+    const { swrKeyMap } = get()
+    const swrKey = swrKeyMap.get(action)
     mutate(swrKey, data, opts)
   },
 }))
